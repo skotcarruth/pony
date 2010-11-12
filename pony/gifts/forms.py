7aaa775eeb27d8dbbf8cbd5c3c4bb1ec24ebeea1
Jeff Schenck <jmschenck@gmail.com>
2010-11-12 01:30:01 -0800

    Updates form styles

--- pony/gifts/forms.py
+++ pony/gifts/forms.py
@@ -5,6 +5,11 @@ from pony.gifts.models import Gift
 
 class GiftForm(forms.ModelForm):
     """Form for creating a new gift request."""
+    name = forms.CharField(label='What I Want')
+    price = forms.CharField(label='What It Costs')
+    image = forms.ImageField(label='What It Looks Like', required=False)
+    description = forms.CharField(label='Why I Want It', widget=forms.Textarea, required=False)
+
     class Meta:
         model = Gift
-        fields = ('name', 'description', 'price', 'image',)
+        fields = ('name', 'price', 'image', 'description',)
