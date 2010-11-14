89423defde9705795ed485345a8dc17551b351d9
Jeff Schenck <jmschenck@gmail.com>
2010-11-13 18:52:48 -0800

    Improves form styling

--- pony/static/js/base.js
+++ pony/static/js/base.js
@@ -18,4 +18,18 @@
     return parsed;
   };
 
+  pony.style_file_inputs = function() {
+    // Crazy js fakery to mimic styles on file inputs
+    var file_inputs = $('input[type=file]').addClass('file_input');
+    // Set up the wrapping html
+    file_inputs.wrap('<div class="file_input"></div>');
+    file_inputs.after('<div class="fake_file_input"><input type="text" disabled="disabled" /><span>Browse...</span></div>');
+    // Set up the onchange event to update the fake input
+    file_inputs.change(function() {
+      var file_input = $(this);
+      file_input.parents('div.file_input').find('div.fake_file_input input').val(file_input.val());
+    });
+  };
+  $(pony.style_file_inputs);
+
 })(jQuery);
